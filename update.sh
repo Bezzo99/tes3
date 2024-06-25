e691fcb69ed5193410d9be068a901ba072c93f6f
Bezzo99 <bezzoperantau9699@gmail.com>
2024-06-25 12:55:40 +0700

    bebas ya

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://Bezzo99.github.io/limit/menu.zip
+    wget https://raw.githubusercontent.com/Bezzo99/tes3/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
